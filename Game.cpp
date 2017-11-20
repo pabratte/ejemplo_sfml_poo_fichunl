f07e92826bf43431c2354b9707b143efe7801d6f
Pablo Abratte <pabratte@gmail.com>
2017-11-20 12:05:57 -0300

    Personaje con movimiento

--- Game.cpp
+++ Game.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Window/Event.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/Keyboard.hpp>
 
 Game::Game(){
 	init();
@@ -10,6 +11,9 @@ Game::Game(){
 void Game::init(){
 	// crear la ventana de juego
 	w.create(sf::VideoMode(640,400),"Ejemplo de SFML");
+	
+	// limitar la cantidad de actualizaciones por segundo
+	w.setFramerateLimit(60);
 	
 	// cargar las texturas
 	texBackground.loadFromFile("assets/images/background.png");
@@ -44,7 +48,18 @@ void Game::run(){
 
 
 void Game::update(){
+	// mover al personaje
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+		spChar.move(-5, 0);
+	}else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+		spChar.move(5, 0);
+	}
 	
+	// limitar el movimiento del personaje a la ventana
+	sf::Vector2f charPos = spChar.getPosition();
+	if(charPos.x < 0) charPos.x = 0;
+	if(charPos.x > 608) charPos.x = 608;
+	spChar.setPosition(charPos);
 }
 
 void Game::draw(){
